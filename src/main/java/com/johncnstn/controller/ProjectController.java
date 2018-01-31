13f71df58300b0cfdcf37a8acf3145996772be9c
JohnCnstn <crystalfaster@gmail.com>
2018-01-31 10:03:19 +0300

    refactor

--- src/main/java/com/johncnstn/controller/ProjectController.java
+++ src/main/java/com/johncnstn/controller/ProjectController.java
@@ -1,15 +1,25 @@
 package com.johncnstn.controller;
 
+import com.johncnstn.data.dto.ProjectDto;
+import com.johncnstn.data.entity.Project;
+import com.johncnstn.data.service.ManagerService;
 import com.johncnstn.data.service.ProjectService;
+import com.johncnstn.data.service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class ProjectController {
+
+    @Autowired
+    private WorkerService workerService;
+    @Autowired
+    private ManagerService managerService;
 
     @Autowired
     private ProjectService projectService;
@@ -22,7 +32,21 @@ public class ProjectController {
     @GetMapping("/updateProject/{id}")
     public String getUpdateProject(@PathVariable(value = "id") Long id, Model model) {
         model.addAttribute("project", projectService.findOne(id));
+        model.addAttribute("managerList", managerService.findAll());
+        model.addAttribute("workerList", workerService.findAll());
         return "/updateProject";
+    }
+
+    @PostMapping("/updateProject")
+    public String updateProject(@Valid @ModelAttribute("project") Project project, BindingResult result) {
+        if (!result.hasErrors()) {
+//            createProject(projectDto, result);
+        }
+        if (result.hasErrors()) {
+            return "registration";
+        } else {
+            return "redirect:/home";
+        }
     }
 
     @DeleteMapping("/project/{id}")
@@ -30,6 +54,10 @@ public class ProjectController {
         model.addAttribute("project", projectService.findOne(id));
         projectService.delete(id);
         return "redirect:/home";
+    }
+
+    private void createProject(ProjectDto projectDto, BindingResult result) {
+        projectService.createNewProject(projectDto);
     }
 
 }
--- src/main/java/com/johncnstn/data/dto/ProjectDto.java
+++ src/main/java/com/johncnstn/data/dto/ProjectDto.java
@@ -32,4 +32,6 @@ public class ProjectDto {
     @Getter
     private List<Long> workerId;
 
+    
+
 }
