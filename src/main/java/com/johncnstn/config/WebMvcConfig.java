fd77b343191ef028bf9e2888583d2a0ed902b787
Pavel <crystalfaster@gmail.com>
2018-02-08 15:58:44 +0300

    add table from json react (hard coded)

--- src/main/java/com/johncnstn/config/WebMvcConfig.java
+++ src/main/java/com/johncnstn/config/WebMvcConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
