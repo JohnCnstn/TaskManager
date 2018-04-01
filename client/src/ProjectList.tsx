bf93768c7c1704742b203ec667dea8e4c417f980
Pavel Khankevich <crystalfaster@gmail.com>
2018-04-01 03:16:23 +0300

    react 1st step

---
+++ client/src/ProjectList.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+interface Project {
+    id: number;
+    name: string;
+    description: string;
+}
+
+interface ProjectListProps {
+
+}
+
+class ProjectList extends React.Component<{}, any> {
+    constructor(props: ProjectListProps) {
+        super(props);
+
+        this.state = {
+            projects: [],
+            isLoading: false
+        };
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+
+        fetch('http://localhost:8080/projects')
+            .then(response => response.json())
+            .then(data => this.setState({projects: data, isLoading: false}));
+    }
+
+    render() {
+        const {projects, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div>
+                <h2>Project List</h2>
+                {projects.map((project: Project) =>
+                    <div key={project.id}>
+                        {project.name}:
+                        {project.description}
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+export default ProjectList;
