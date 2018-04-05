21ce6cbe318407bedf013867627fe09498653309
Pavel Khankevich <crystalfaster@gmail.com>
2018-04-06 01:52:53 +0300

    add edit for all fields

--- client/src/Task.tsx
+++ client/src/Task.tsx
@@ -22,6 +22,9 @@ class Task extends React.Component<TaskProps, any> {
 
         this.state = {
             tasks: [],
+            idForEdit: -1,
+            txtForEdit: 'edited txt',
+            edit: false,
             isLoading: false
         };
     }
@@ -34,6 +37,24 @@ class Task extends React.Component<TaskProps, any> {
             .then(response => response.json())
             .then(data => this.setState({tasks: data, isLoading: false}));
     }
+
+    editTask(id: number) {
+
+        var text = this.state.txtForEdit;
+
+        let arr = this.state.tasks;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].id === id) {
+                let editedTask  = arr[i];
+                editedTask.name  = text;
+                editedTask.path  = text;
+            }
+        }
+
+        this.setState({tasks: arr, edit: false});
+    }
+
     newTask(text: string) {
         let arr = this.state.tasks;
         let newTask = new NewTask();
@@ -48,6 +69,7 @@ class Task extends React.Component<TaskProps, any> {
         arr.push(newTask);
         this.setState({tasks: arr});
     }
+
     removeTask(id: number) {
         let arr = this.state.tasks;
 
@@ -58,30 +80,52 @@ class Task extends React.Component<TaskProps, any> {
         }
         this.setState({tasks: arr});
     }
-    render() {
 
-        const {tasks, isLoading} = this.state;
+    rendEdit(id: number) {
+        return (
+            <div className="box">
+                <input type="text" onChange={this.changeEditedText.bind(this, event)} value={this.state.txtForEdit}/>
+                <button onClick={this.editTask.bind(this, id)}>edit</button>
+            </div>
+        );
+    }
 
-        if (isLoading) {
-            return <p>Loading...</p>;
+    changeEditedText(event: Event) {
+        if (event.target != null) {
+            this.setState({value: event.target.valueOf()});
         }
+    }
 
+    setEdit(id: number) {
+        this.setState({edit: true, idForEdit: id});
+    }
+
+    rendNorm() {
         return (
             <div>
                 <h2>Tasks</h2>
 
                 <button onClick={this.newTask.bind(this, 'custom data')}>add</button>
 
-                {tasks.map((task: TaskDto) =>
+                {this.state.tasks.map((task: TaskDto) =>
                     <div key={task.id}>
                         {task.id}:
                         {task.name}:
                         {task.path}
+                        <button onClick={this.setEdit.bind(this, task.id)}>edit</button>
                         <button onClick={this.removeTask.bind(this, task.id)}>remove</button>
                     </div>
                 )}
             </div>
         );
+    }
+
+    render() {
+        if (this.state.edit) {
+            return this.rendEdit(this.state.idForEdit);
+        } else {
+            return this.rendNorm();
+        }
     }
 }
 
